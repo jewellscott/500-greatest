0d43e510f647025311a7359eb73f7f36f58b8677
Jewell Scott <jewelljrscott@gmail.com>
2025-02-20 19:07:58 -0500

    Add album randomization

--- pages/home.php
+++ pages/home.php
@@ -6,6 +6,8 @@
 
 	Show the latest album you randomized, and the option to generate another. Maybe also show the percentage completed. 
 
+	<?php include('templates/modules/random-album/template.php'); ?>
+
 <?php } else { ?>
 
 	Show some spiel about the app.
---
+++ templates/modules/random-album/template.php
@@ -0,0 +1,49 @@
+<?php 
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	// in the future, it'll pull just from albums you HAVEN'T listened to
+
+	$albums = file_get_contents('data/albums.json');
+	$albums = json_decode($albums, true);
+
+	function getRandomAlbum() {
+
+		// get all albums
+		// i had to initialize this stuff again... it wouldn't work otherwise
+
+			$albums = file_get_contents('data/albums.json');
+			$albums = json_decode($albums, true);
+
+		// get a random one
+
+		$key = array_rand($albums, 1);
+
+		$album = $albums[$key];
+
+		$_SESSION['random-album'] = $album;
+
+		return $album;
+	}
+
+	$album = $_SESSION['random-album'] ?? getRandomAlbum();
+
+	if (isset($_POST['get-random'])) {
+		$_SESSION['random-album'] = null;
+		// i had to add this! couldn't just ovewrite it
+
+   	$album = getRandomAlbum();
+   	$_SESSION['random-album'] = $album;
+   }
+
+ ?>
+
+ <random-album>
+ 	<?php include('templates/modules/album-card/template.php'); ?>
+
+ 	<form method="POST">
+ 		<button type="submit" name="get-random" action="">Randomize</button>
+ 	</form>
+ </random-album>
