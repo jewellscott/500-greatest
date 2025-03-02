10e54d638bb054c2c1de3b33360eeefa666d663f
Jewell Scott <jewelljrscott@gmail.com>
2025-03-02 10:16:04 -0500

    Add user stats

--- pages/my-stats.php
+++ pages/my-stats.php
@@ -8,8 +8,48 @@
 			SELECT * FROM users 
 			WHERE id = $userId")->fetch();
 
+    
+
+
+
+   $userStats = getUserStats($db, $userId);
+
+	$listenedCount = $userStats["listenedCount"];
+	$percentageComplete = $userStats["percentageComplete"];
+	$averageRating = $userStats["averageRating"];
+	$topDecade = $userStats["topDecade"];
+	$topGenres = $userStats["topGenres"];
+	$topArtists = $userStats["topArtists"];
+	$topAlbums = $userStats["topAlbums"];
+
+
  ?>
 
 <h1><?=$user["username"]?>'s Stats</h1>
 
 just loop through all of the attributes of a huge user stats object
+
+<ul>
+	<li>listenedCount: <?=$listenedCount?></li>
+	<li>percentComplete: <?=$percentageComplete?></li>
+	<li>averageRating: <?=$averageRating?></li>
+	<li>topDecade: <?=$topDecade?></li>
+	<li>topGenres:</li>
+	<ul>
+		<?php foreach($topGenres as $genre) { ?>
+			<li><?=$genre?></li>
+		<?php } ?>
+	</ul>
+		<li>topArtists:</li>
+	<ul>
+		<?php foreach($topArtists as $artist) { ?>
+			<li><?=$artist?></li>
+		<?php } ?>
+	</ul>
+	<li>topAlbums:</li>
+	<ul>
+		<?php foreach($topAlbums as $album) { 
+			 include('..templates/components/album-card');
+		 } ?>
+	</ul>
+</ul>
--- stats.php
+++ stats.php
@@ -1,19 +1,15 @@
 <?php 
 global $db; 
 
-// $user = $db->query("
-// 	SELECT * FROM users 
-// 	WHERE id = $userId")->fetch();
-
 $userId = $_SESSION["user"]["id"];
 
 
-function getUserStats($userId) {
+function getUserStats($db, $userId) {
 	global $db; 
 	
 	$userStats = [
 		'listenedCount' => 0,
-		'percentComplete' => 0,
+		'percentageComplete' => 0,
 		'averageRating' => 0,
 		'topDecade' => '',
 		'topGenres' => [],
@@ -23,13 +19,73 @@ function getUserStats($userId) {
 
 	// a bunch of queries 
 
-    $stmt = $db->prepare("SELECT COUNT(*) FROM reviews WHERE user_id = ?");
-    // $stmt->execute([$userId]);
-    // $stats['listenedCount'] = $stmt->fetchColumn();
+	// listenedCount
 
-	var_dump($userId);
+    $stmt = $db->prepare("SELECT COUNT(*) FROM reviews WHERE user_id = ?");
+    $stmt->execute([$userId]);
+    $userStats['listenedCount'] = $stmt->fetchColumn();
+
+	// percentComplete
+
+    $stmt = $db->prepare("SELECT COUNT(*) FROM reviews WHERE user_id = ?");
+    $stmt->execute([$userId]);
+    $userStats['percentageComplete'] = ($stmt->fetchColumn() / 500 * 100);
+
+   // averageRating
+
+   $stmt = $db->prepare("SELECT AVG(rating) FROM reviews WHERE user_id = ?");
+	$stmt->execute([$userId]);
+	$userStats['averageRating'] = $stmt->fetchColumn();
+
+   // topDecade
+
+   $stmt = $db->prepare("
+    	SELECT FLOOR(albums.year / 10) * 10 AS decade, 
+   		COUNT(*) AS count 
+   	FROM reviews 
+  		JOIN albums ON reviews.album_id = albums.id   	
+  		WHERE user_id = ? 
+   	GROUP BY decade
+   	ORDER BY count DESC
+   	LIMIT 1;");
+	$stmt->execute([$userId]);
+	$userStats['topDecade'] = $stmt->fetchColumn();
+	// is not actually returning the top decade
+	// rounding error that I don't know how to fix
+
+   // topGenres
+
+   $stmt = $db->prepare("
+		SELECT genre, COUNT(*) AS frequency
+   	FROM reviews 
+   	JOIN albums ON reviews.album_id = albums.id
+   	WHERE user_id = ? 
+   	GROUP BY genre
+		ORDER BY frequency DESC
+		LIMIT 3;");
+	$stmt->execute([$userId]);
+	$userStats['topGenres'] = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+   // topArtists
+
+     $stmt = $db->prepare("
+		SELECT artist, COUNT(*) AS frequency
+   	FROM reviews 
+   	JOIN albums ON reviews.album_id = albums.id
+   	WHERE user_id = ? AND rating >= 4.5
+   	GROUP BY artist
+		ORDER BY frequency DESC
+		LIMIT 10;");
+	$stmt->execute([$userId]);
+	$userStats['topArtists'] = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+
+
+   // topAlbums
 
 	return $userStats;
 }
 
-getUserStats($userId);
+// $userStats = getUserStats($db, $userId);
+
+// $listenedCount = $userStats["listenedCount"];
+
